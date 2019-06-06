e9b1387df7b64212f7cd055a8f1331db8f033c8e
liupeng <LIUPENG.0@outlook.com>
2019-06-06 23:37:56 +0800

    全他妈瞎几把的错误，操！

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,18 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Home from '../views/Home'
 import About from '../views/About.vue'
-
+// 导入vue页面
+// eslint-disable-next-line no-unused-vars
+import RouterA from '../views/routerA'
+// eslint-disable-next-line no-unused-vars
+import RouterB from '../views/routerB'
+// 使用路由
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/HelloWorld',
+      path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     },
@@ -22,6 +27,17 @@ export default new Router({
       path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/RouterA',
+      component: 'RouterA'
+    },
+    {
+      name: 'RouterB',
+      // path: '/RouterB',
+      // 动态拼接路由path，如果不指定url只是路由页面，否则（/RouterB/name2）
+      path: '/RouterB/:name',
+      component: 'RouterB'
     }
   ]
 })
