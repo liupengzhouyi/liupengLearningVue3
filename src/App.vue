304933eff7497b356d16a94519c4ba75a80ca02f
liupeng <LIUPENG.0@outlook.com>
2019-06-08 23:18:26 +0800

    循环嵌套 V 1.0.1

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,26 @@
       </ol>
     </h3>
 
+    <ol>
+      <li v-for="item in list1">
+        {{ item.name1 }}
+      </li>
+    </ol>
+
+    <hr>
+
+    <ul>
+      <li v-for="oo in list2">
+        {{ oo.cate }}
+
+        <ol>
+          <li v-for="ooo in oo.list">
+            {{ ooo.title }}
+          </li>
+        </ol>
+      </li>
+    </ul>
+
     <router-view/>
   </div>
 </template>
@@ -36,7 +56,29 @@ export default { /* export default 表示把这个组件暴露出去 */
       obj: {
         name: '张三'
       },
-      list: ['11', '22', '33']
+      list: ['11', '22', '33'],
+      list1: [
+        {'name1':'liupeng01'},
+        {'name1':'liupeng02'},
+        {'name1':'liupeng03'},
+        {'name1':'liupeng04'},
+      ],
+      list2: [
+        {
+          "cate": "国内新闻",
+          "list": [
+            {'title': 'news11'},
+            {'title': 'news12'}
+          ]
+        },
+        {
+          "cate": "国外新闻",
+          "list": [
+            {'title': 'news111'},
+            {'title': 'news112'}
+          ]
+        }
+      ]
     }
   }
 }
