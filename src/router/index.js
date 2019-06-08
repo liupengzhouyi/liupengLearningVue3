53e5b1d71de517ec104229a5d8e054de1f93a14c
liupeng <LIUPENG.0@outlook.com>
2019-06-08 21:45:19 +0800

    导入项目，还是全他妈瞎几把的错误，操！

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Home from '../views/Home'
-import About from '../views/About.vue'
-// 导入vue页面
-// eslint-disable-next-line no-unused-vars
-import RouterA from '../views/routerA'
-// eslint-disable-next-line no-unused-vars
-import RouterB from '../views/routerB'
-// 使用路由
+import home from '@/components/home'
+import page01 from '@/components/page01'
+import page02 from '@/components/page02'
+import page01A from '@/components/page01/page01-A'
+import page01B from '@/components/page01/page01-b'
+import pageEnd from '@/components/page01/B/end'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
+      name: 'page01',
+      component: page01
     },
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: home
     },
     {
-      path: '/RouterA',
-      component: 'RouterA'
+      path: '/page01',
+      name: 'page01',
+      component: page01,
+      children: [
+        {
+          path: 'page01-a',
+          name: 'page01A',
+          component: page01A
+        },
+        {
+          path: 'page01-b',
+          name: 'page01B',
+          component: page01B,
+          children: [
+            {
+              path: 'end',
+              name: 'pageEnd',
+              component: pageEnd
+            }
+          ]
+        }
+      ]
     },
     {
-      name: 'RouterB',
-      // path: '/RouterB',
-      // 动态拼接路由path，如果不指定url只是路由页面，否则（/RouterB/name2）
-      path: '/RouterB/:name',
-      component: 'RouterB'
+      path: '/page02',
+      name: 'page02',
+      component: page02
     }
   ]
 })
